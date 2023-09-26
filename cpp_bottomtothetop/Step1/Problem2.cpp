d84001e6714075cd703e9b0fea870ddd57a7da37
spbabodev <107238685+spbabodev@users.noreply.github.com>
2023-09-26 20:31:42 +0900

    20230926
    구색맞추기;;

--- cpp_bottomtothetop/Step1/Problem2.cpp
+++ cpp_bottomtothetop/Step1/Problem2.cpp
@@ -1,10 +1,10 @@
-#include <stdio.h>
-
-int main() {
-	int a, b;
-
-	scanf_s("%d %d", &a, &b);
-	printf("%d", a + b);
-
-	return 0;
-}
+//#include <stdio.h>
+//
+//int main() {
+//	int a, b;
+//
+//	scanf("%d %d", &a, &b);
+//	printf("%d", a + b);
+//
+//	return 0;
+//}
